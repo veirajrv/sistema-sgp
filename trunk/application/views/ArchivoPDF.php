9efc981dd24d95693f0665fe999cd968ecbfe85c
jrodriguezv.11 <jrodriguezv.11@0883cc14-8b38-579a-34fb-d7a1dfdec66d>
2012-02-13 23:29:11 +0000

    Base de datos nueva montada en el sistema

--- trunk/application/views/ArchivoPDF.php
+++ trunk/application/views/ArchivoPDF.php
@@ -2,8 +2,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<script src="http://elp21.no-ip.info:4085/SGP/files/js/jquery-1.6.2.min.js" type="text/javascript"> </script>
-<script src="http://elp21.no-ip.info:4085/SGP/files/js/jquery-ui-1.8.16.custom.min.js" type="text/javascript"> </script>
+<script src="<?php echo base_url();?>files/js/jquery-1.6.2.min.js" type="text/javascript"> </script>
+<script src="<?php echo base_url();?>files/js/jquery-ui-1.8.16.custom.min.js" type="text/javascript"> </script>
 <head>
 
 <script>
